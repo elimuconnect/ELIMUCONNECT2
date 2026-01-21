23c9e2bda49d1bd0cadd5b26e542f7e116f4d11f
elimuconnect <wamithindegwa1@gmail.com>
2026-01-21 12:10:37 +0300

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-<script type="module">
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-  export const supabase = createClient(
-    "https://zcmzuetusvpmvubbjlmx.supabase.co'",
-    "sb_publishable_-BGJLAySaH7bQ5lBIZF9zg_eohK-xEn"
-  );
-</script>
